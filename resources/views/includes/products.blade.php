e344f520561e925818ab7468531188758f33e84a
Hasan <hasanismaiel19951995@gmail.com>
2025-05-22 10:19:11 +0300

    change in products

--- resources/views/includes/products.blade.php
+++ resources/views/includes/products.blade.php
@@ -15,8 +15,8 @@
         <div class="mb-5">
             <h4 class="mb-4 text-center fw-bold border-bottom pb-2">Hair Care</h4>
             <div class="row gx-3 gy-4 justify-content-center">
-                @foreach ($haircare_products as $product)
-                <div class="col-lg-2 col-md-4 col-sm-6">
+                @foreach ($haircare_products as $index => $product)
+                <div class="col-lg-2 col-md-4 col-sm-6 haircare-product {{ $index >= 6 ? 'd-none' : '' }}">
                     <a href="{{ route('product', [$product->id]) }}" class="d-block text-center">
                         <div class="card border-0 shadow-sm">
                             <img src="{{ asset($product->image) }}" alt="Hair Care Product" class="img-fluid" style="aspect-ratio: 1 / 1; object-fit: cover;" loading="lazy">
@@ -28,6 +28,11 @@
                 </div>
                 @endforeach
             </div>
+            @if ($haircare_products->count() > 6)
+            <div class="text-center mt-3">
+                <button class="btn btn-outline-primary" onclick="toggleProducts('haircare-product', this)">See More</button>
+            </div>
+            @endif
         </div>
         @endif
 
@@ -42,6 +47,12 @@
                         <div class="card border-0 shadow-sm">
                             <img src="{{ asset($product->image) }}" alt="Skin Care Product" class="img-fluid" style="aspect-ratio: 1 / 1; object-fit: cover;" loading="lazy">
                         </div>
+                        <div class="card border-0 shadow-sm">
+                            <img src="{{ asset($product->image) }}" alt="Hair Care Product" class="img-fluid" style="aspect-ratio: 1 / 1; object-fit: cover;" loading="lazy">
+                            <div style="text-align: left;" class="ms-2 mb-2">{{$product->type}}</div>
+                            <div style="text-align: left;" class="ms-2 mb-2">{{$product->flavor}}</div>
+                            <div style="text-align: left;" class="ms-2 mb-2">{{$product->size}}</div>
+                        </div>
                     </a>
                 </div>
                 @endforeach
@@ -52,3 +63,21 @@
     </div>
 </section>
 <!-- /product Section -->
+
+
+<!-- see more products -->
+<script>
+    function toggleProducts(className, btn) {
+        const items = document.querySelectorAll('.' + className);
+        let isExpanded = btn.dataset.expanded === 'true';
+
+        items.forEach((item, index) => {
+            if (index >= 6) {
+                item.classList.toggle('d-none', isExpanded);
+            }
+        });
+
+        btn.textContent = isExpanded ? 'See More' : 'See Less';
+        btn.dataset.expanded = !isExpanded;
+    }
+</script>
